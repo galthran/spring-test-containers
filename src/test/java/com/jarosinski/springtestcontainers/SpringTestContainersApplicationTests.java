0164ab1e0d131b15df36ab5f1bb67c789cdbd42f
galthran80 <rafal.jarosinski@gmail.com>
2022-02-02 21:46:09 +0100

    Initial commit

---
+++ src/test/java/com/jarosinski/springtestcontainers/SpringTestContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarosinski.springtestcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestContainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
