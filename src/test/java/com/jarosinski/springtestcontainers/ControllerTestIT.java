3cbe7dc9c93fa3e5b86319940a9a6ee7f054bb93
galthran80 <rafal.jarosinski@gmail.com>
2022-02-09 20:02:35 +0100

    Creating and using postgresql test container

---
+++ src/test/java/com/jarosinski/springtestcontainers/ControllerTestIT.java
@@ -0,0 +1,38 @@
+package com.jarosinski.springtestcontainers;
+
+import org.junit.ClassRule;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+@ActiveProfiles("test")
+class ControllerTestIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @ClassRule
+    public static PostgreSQLContainer postgreSQLContainer = PostgresqlContainer.getInstance();
+
+    @Test
+    void getPeople() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+}
